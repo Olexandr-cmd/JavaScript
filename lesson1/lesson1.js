9208aba72cd47da91bfc578f622f5cbb1d8f894f
SashaKruts <disaster1209>
2020-10-29 14:38:08 +0200

    lesson1

---
+++ lesson1/lesson1.js
@@ -0,0 +1,371 @@
+//  Завдання 1
+// let textOne = "Hello";
+// let textTwo = "owu";
+// let textThree = "com";
+// let textFour = "ua";
+// let figureOne = 1;
+// let figureTwo = 10;
+// let figureThree = -999;
+// let figureFour = 123;
+// let figureFive = 3.14;
+// let figureSix = 2.7;
+// let figureSeven = 16;
+// let bool = true;
+// let bool = false;
+// console.log(textOne, textTwo, textThree, textFour);
+// alert(figureOne);
+// alert(figureTwo);
+// alert(figureThree);
+// alert(figureFour);
+// alert(figureFive);
+// alert(figureSix);
+// alert(figureSeven);
+// document.write(bool);
+// document.write("<br>");
+//  document.write(bool);
+//
+
+//
+// Завдання 2
+// let textOne = "sasha";
+// let textTwo = "home";
+// let textThree = "work";
+// let textFour = "usa";
+// let figureOne = 3;
+// let figureTwo = 4;
+// let figureThree = 999;
+// let figureFour = 623;
+// let figureFive = 7.14;
+// let figureSix = 8.7;
+// let figureSeven = 46;
+// let bool = false;
+// let bool = true;
+// console.log(textOne, textTwo, textThree, textFour);
+// alert(figureOne);
+// alert(figureTwo);
+// alert(figureThree);
+// alert(figureFour);
+// alert(figureFive);
+// alert(figureSix);
+// alert(figureSeven);
+// document.write(bool);
+// document.write("<br>");
+// document.write(bool);
+//
+
+// Завдання 3
+// let name = "Sasha";
+// let age = 21;
+// let build = "man";
+// alert(name);
+// document.write(age);
+// console.log(build);
+// let height = 181;
+// let from = "ukraine";
+// let weight = 78;
+// alert(from);
+// document.write(height);
+// console.log(weight);
+//
+
+// Завдання 4 5 6
+// let name = prompt("What is your name?");
+// let surname = prompt("What is your surname");
+// let nameTwo = prompt("What is your nameTwo");
+// console.log(name);
+// document.write(surname);
+// alert(nameTwo);
+// console.log(nameTwo);
+// document.write(name);
+// alert(surname);
+//
+// console.log(surname);
+// document.write(nameTwo);
+// alert(name);
+// let name = prompt("What is your name?");
+// let surname = prompt("What is your surname");
+// let nameTwo = prompt("What is your nameTwo");
+// let person = name +" "+ surname +" "+ nameTwo;
+//let name = prompt("What is your name?");
+// let surname = prompt("What is your surname");
+// let nameTwo = prompt("What is your nameTwo");
+// let myMom = name +" "+ surname +" "+ nameTwo;
+// let name = prompt("What is your name?");
+// let surname = prompt("What is your surname");
+// let nameTwo = prompt("What is your nameTwo");
+// let mySister = name +" "+ surname +" "+ nameTwo;
+//
+
+// Завдання 7
+// let num1 = prompt("figure");
+// console.log(+num1);
+// let num2 = prompt("figure");
+// console.log(+num2);
+// let num3 = prompt("figure");
+// console.log(+num3);
+
+//Завдання 8
+// let num1 = prompt("figure");
+// let num2 = prompt("figure2");
+// let num3 = prompt("figure3");
+// let num4 = prompt("figure4");
+// let result = (parseInt(num1)) + (parseInt(num2)) + (parseInt(num3)) + (parseInt(num4));
+// console.log(result);
+
+// Завдання 9
+// let num1 = prompt("Перше число");
+// let num2 = prompt("Друге число");
+// let num3 = prompt("Третє число");
+// let result = (parseFloat(num1)) + (parseFloat(num2)) + (parseFloat(num3));
+// console.log(result);
+
+// Завдання 10
+// let num1 = prompt("Перше число");
+// let num2 = prompt("Друге число");
+// let num3 = prompt("Третє число");
+// let result = Math.round(num1) + Math.round(num2) + Math.round(num3);
+// console.log(result);
+//
+
+// Завдання 11
+// let num1 = prompt("Перше число");
+// let num2 = prompt("Друге число");
+// console.log(Math.pow(parseInt(num1),parseInt(num2)));
+//
+
+// Завдання 12
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+//
+
+// Завдання 13
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 == 6 -> false
+// 5 >= 6 -> false
+// 10 == 10 -> true
+// 10 >= 10 -> true
+// 10 < 10 -> false
+// 10 > 10 -> false
+// 10 <= 10 -> false
+// 123 === '123' -> false
+// 123 == '123' -> true
+//
+
+// Завдання 14
+// console.log(132 > 100 && 45 < 12 ); false
+// console.log(34 > 33 && 23 < 90 ); true
+// console.log(99 > 100 && 45 > 12 ); false
+// console.log(132 > 100 || 45 < 12 ); true
+// console.log(111 > 11 || 45 < 111 ); true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); true
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); true
+// console.log(!!'-1'); true
+// console.log(!!-1); true
+// console.log(!!'0'); true
+// console.log(!!'null'); true
+// console.log(!!'undefined'); true
+// console.log(!!(3/'owu')); false
+// console.log((111 > 11 || 45 < 111) ||  !!'0'); true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); false
+//
+
+// Class
+
+// Завдання 1
+// let str = "Привет";
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
+//
+
+// Завдання 2 - 3
+// let a1 = 5+ 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+// let a5 = 5 % 3;
+//
+// let a6 = 5 % 3; 2
+// let a7 = 3 % 5; 3
+// let a8 = 5 + '3'; 53
+// let a9 = '5' - 3; 2
+// let a10 = 75 + 'кг'; 75
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+//
+
+// Завдання 4
+// let height = 23;
+// let wight = 10;
+// let s = (height * wight);
+// console.log(s);
+//
+
+// Завдання 5
+// let heightC = 10;
+// let dC = 4;
+// let v = Math.PI * Math.pow ((dC/2),2) * heightC;
+// console.log(v);
+//
+
+// Завдання 6
+// let n = 3;
+// let m = 4;
+// let k = Math.sqrt(n**2 + m**2);
+// console.log(k);
+
+// Завдання 7
+// let str = "Hello World";
+// document.write(str);
+// alert(str);
+// console.log(str);
+
+// Завдання 8
+// let NameAgeHobby = "Круць Олександр Юрійович\n21\nIt";
+// alert(NameAgeHobby);
+//
+
+// Завдання 9
+// let str1 = "Кто";
+// let str2 = "ты";
+// let str3 = "такой?";
+// let concatenation = str1 +" "+ str2 +" "+ str3;
+// document.write(concatenation);
+//
+
+// Завдання 10
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); 205
+// document.write(str - a + "<br/>"); 15
+// document.write(str * "2" + "<br/>"); 40
+// document.write(str / 2 + "<br/>"); 10
+//
+
+// Завдання 11
+// parseInt("3.14"); 3
+// parseInt("-7.875"); -7
+// parseInt("435"); 435
+// parseInt("Вася"); NaN
+//
+
+// Завдання 12
+// let str = prompt("Ваш код", "123")
+// console.log(str);
+//
+
+// Завдання 13
+// let num1 = prompt("figure1");
+// let num2 = prompt("figure2");
+// let result = (parseInt(num1)) + (parseInt(num2));
+// alert(result);
+//
+
+// Завдання 14
+// let name = prompt("What is your name?");
+// let surname = prompt("What is your surname?");
+// let age = prompt("How old are you?");
+// let  isIvanIvanov = (name === "Иван" && (surname === "Иванов" && (age == 32)));
+// if (isIvanIvanov);
+// {     alert("Доброго вечера Иван Иванов, мои поздравления что вам 32");
+// }
+//
+
+
+// ДОП
+
+// Завдання 1
+// let num1= prompt("number1");
+// let num2= prompt("number2");
+// let num3= prompt("number3");
+// if(num2 < num1 && num2 < num3){
+//     if(num1<num3) {
+//         document.write(num2, num1, num3);
+//     }
+//     else{
+//         document.write(num2, num3, num1);
+//     }
+// }
+// else if(num3 < num1 && num3 <num2){
+//     if(num2<num1){
+//         document.write(num3, num2, num1);
+//     }
+//     else{
+//         document.write(num3, num1, num2)
+//     }
+// }
+// else if(num1 < num3 && num1 < num2){
+//     if(num3<num2){
+//         document.write(num1, num3, num2);
+//     }
+//     else{
+//         document.write(num1, num2, num3)
+//     }
+// }
+// else{
+//     document.write("Unknown number")
+// }
+
+// Завдання 2
+// let color = prompt("Виберіть колір: зелений, жовтий, червоний або зламався");
+// if (color === "зелений") {
+//     alert("Йди")
+// }
+// else if (color === "жовтий"){
+//     alert("Зачекай")
+// }
+// else if (color === "червоний"){
+//     alert("Стій")
+// }
+// else if (color === "зламався"){
+//     alert("Роби що хочеш")
+// }
+//
+
+// Завдання 3
+// let color = prompt("Виберіть колір:залений,жовтий,червоний або зламався");
+// let isRoadClear = confirm("Натисніть ок якщо машин немає");
+// if (color === "зелений" && isRoadClear === true)
+// {
+//     alert("йди")
+// }
+// else if (color === "зелений" && isRoadClear === false)
+// {
+//     alert("подожди пока нарушители проедут")
+// }
+// else if (color === "жовтий" && isRoadClear === true)
+// {
+//     alert("жди")
+// }
+// else if (color === "жовтий" && isRoadClear === false)
+// {
+//     alert("все рано жди")
+// }
+// else if (color === "червоний" && isRoadClear === true)
+// {
+//     alert("стой все рано")
+// }
+// else if (color === "червоний" && isRoadClear === false)
+// {
+//     alert("стой и жди")
+// }
+// else
+// {
+//     alert("делай что хочешь")
+// }
